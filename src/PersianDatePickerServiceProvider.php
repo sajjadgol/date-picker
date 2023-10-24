ccb8ad51c13b7b52b7b1a75777412cf268a7023b
sajjadgol <sajjadgol@users.noreply.github.com>
2023-10-24 01:56:14 +0000

    Fix styling

--- src/PersianDatePickerServiceProvider.php
+++ src/PersianDatePickerServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use Sajjadgol\PersianDatePicker\Commands\PersianDatePickerCommand;
+use Sajjadgol\PersianDatePicker\Testing\TestsPersianDatePicker;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Sajjadgol\PersianDatePicker\Commands\PersianDatePickerCommand;
-use Sajjadgol\PersianDatePicker\Testing\TestsPersianDatePicker;
 
 class PersianDatePickerServiceProvider extends PackageServiceProvider
 {
